b57df237ef8f83c6ec1ca11fbf1374fc25f092e8
tejaslankeshwar <your-github-email@example.com>
2025-03-11 01:42:54 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com', 'communionhub.org'],
+    domains: [
+      'res.cloudinary.com',
+      'communionhub.org',
+      'images.unsplash.com',
+      'plus.unsplash.com'
+    ],
     remotePatterns: [
       {
         protocol: 'https',
@@ -13,9 +18,21 @@ const nextConfig = {
         hostname: 'res.cloudinary.com',
         pathname: '/dxqv8mbpg/image/upload/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        pathname: '/**',
+      }
     ],
     minimumCacheTTL: 60,
     formats: ['image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   env: {
     NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
@@ -25,15 +42,13 @@ const nextConfig = {
     removeConsole: process.env.NODE_ENV === 'production',
   },
   experimental: {
-    optimizeCss: {
-      enabled: true,
-      cssModules: true,
-    },
+    optimizeCss: true,
   },
   poweredByHeader: false,
   compress: true,
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
 }
 
 module.exports = nextConfig 
